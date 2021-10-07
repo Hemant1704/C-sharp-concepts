a6195a7c5f80886948202143df4acb3e030d8329
HEMANT CHAUHAN <18bit0032@gmail.com>
2021-10-07 18:58:32 +0530

    User input and output to console.

---
+++ Program1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ Namespace:
+    Collections of various classes is called a namespace for example System is a namespace.
+    Better practice is to make a separate file for a class rather than making the class in 
+    the same file.
+  */
+namespace Lecture_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello {0}",name);
+        }
+    }
+}
