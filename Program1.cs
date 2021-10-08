257987a4e0c215a90cac57c5bc5b8405cf061600
HEMANT CHAUHAN <18bit0032@gmail.com>
2021-10-08 22:29:46 +0530

    Data Types and readme modification

--- Program1.cs
+++ Program1.cs
@@ -12,8 +12,22 @@ namespace Lecture_1
     {
         static void Main(string[] args)
         {
+            // User input taken
             string name = Console.ReadLine();
+
+            // Place holder syntax to print the variable.
             Console.WriteLine("Hello {0}",name);
+
+            /*Basic data types in c#
+                sbyte  :  -128 to 127 signed 8 bit
+                byte   :   0 to 255 unsigned
+                char   :   unicode 16 bit character 
+                short  :   signed 16 bit integer
+                int    :   
+                uint   :
+                long   :
+                ulong  :
+            */
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # C-sharp-concepts
 This repo contains various code files to learn c sharp in structured manner.
+
+## 1. Program1.cs
+
+In this I have mentioned about basic concepts like input/output in c sharp, basic data types
