561c21daba549fc2ec39e89e5c8f0e9524e680ab
HEMANT CHAUHAN <18bit0032@gmail.com>
2021-10-09 13:25:06 +0530

    Completed Program1.cs file

--- Program1.cs
+++ Program1.cs
@@ -23,11 +23,26 @@ namespace Lecture_1
                 byte   :   0 to 255 unsigned
                 char   :   unicode 16 bit character 
                 short  :   signed 16 bit integer
-                int    :   
-                uint   :
-                long   :
-                ulong  :
+                int    :   signed 32 bit integer
+                uint   :   unsigned 32 bit integer
+                long   :   signed 64 bit
+                ulong  :   unsigned 64 bit
+            We also have float and double values which differ in precision.
             */
+
+
+            // Escape sequence : For characters like " we need to add a escape sequence to print it.Example is shown
+            string str = "\"hemant\"";
+            Console.WriteLine(str);
+
+            // Verbatim string: Tells compiler to ignore the escape characters and commpile as a normal string
+            // Example @"//" will give // whereas normal "//" will represent / (used in path definition to a folder)
+            string s1 = @"my name is\\hemant";
+            string s2 = "my name is \\ hemant";
+            Console.WriteLine("With verbatim string @ "+s1);
+            Console.WriteLine("Without verbatim string "+s2);
+
+            // Operators are similar to other languages like Java and C++
         }
     }
 }
