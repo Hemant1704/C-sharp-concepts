1d91696dcbbb646fc15f1c1d68ca3b767c18ad56
HEMANT CHAUHAN <18bit0032@gmail.com>
2021-10-15 23:33:34 +0530

    Updated program4.cs

--- Program4.cs
+++ Program4.cs
@@ -12,58 +12,7 @@ namespace Lecture_1
         static void main(string[] args)
         {
             /*
-        In this we are going to study about very basics of arrays and comments in c sharp
-        to declare an array
-        int[] arr = new int[n]; where n is the size of the array
-        strongly typed : as we can only store int if we declare int
-        they cannot grow in size once initialised.
-
-         */
-            // let us create an array of names of students and try to print it
-            string[] name = new string[3];
-            name[0] = "Hemant";
-            name[1] = "Ram";
-            name[2] = "Shyam";
-            for (int i = 0; i < name.Length; i++)
-            {
-                Console.WriteLine(name[i]);
-            }
-            /*
-        Comments in c sharp 
-        there are single line of comments and multiline then 
-        currently we are writing in multiline comments
-        also i have used comments previously so i think we are familiar with it
-
-         */
-
-            /*
-             if else statements 
-            they are similar to java and c++
-             */
-            //let us take an example
-
-        }
-
-
-
-
-    }
-}
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Lecture_1
-{
-    class Program4
-    {
-
-
-
-        static void main(string[] args)
-        {
-            /*
-            1.
+            a)
             In this we are going to study about very basics of arrays and comments in c sharp
             to declare an array
             int[] arr = new int[n]; where n is the size of the array
@@ -82,7 +31,7 @@ namespace Lecture_1
                 Console.WriteLine(name[i]);
             }
             /*
-             2.
+             b)
               Comments in c sharp 
               there are single line of comments and multiline then 
               currently we are writing in multiline comments
@@ -91,7 +40,7 @@ namespace Lecture_1
             */
 
             /*
-            3.
+            c)
             operators are ==> || and &&
             something new is | this single pipe it checks both condition even in or condition 
             but this will not make an impact on the output instead it increases are work.
@@ -102,7 +51,7 @@ namespace Lecture_1
 
 
             /*
-            4.
+            d)
             if else statements 
             they are similar to java and c++
             let us take an example
@@ -117,7 +66,7 @@ namespace Lecture_1
                 Console.WriteLine("var was false");
             }
 
-            // 5. switch case statements are same as java and c++
+            // e) switch case statements are same as java and c++
             int val = 1;
             switch (val) {
                 case 1:
