886dc843a3f2df6cc7ef919c94bc5115b5af5687
HEMANT CHAUHAN <18bit0032@gmail.com>
2021-10-17 23:38:38 +0530

    Added program7.cs complete updated readme as well

--- Program7.cs
+++ Program7.cs
@@ -8,6 +8,18 @@ namespace Lecture_1
 		Namespaces : They help us to organise our code. Example suppose i have 2 teams working on a project and
 		they are working on 2 different things - TeamA and TeamB. So i want to distinguish that which class is 
 		made by which team. With the help of namespaces i can do that easily.
+
+		For example Console class comes from the System namespace which is used to print something on the console.
+		A namespace is nothing but just something that holds up various classes.
+		A namespace can contain:
+		a) namespace
+		b) classes
+		c) interface
+		d) enums
+		e) struct
+		f) delegate.
+
+		This is a little complex topic we will learn it slowly by time. Thus i am not adding anything in the main method.
 		 */
 		static void Main(string[] args) { 
 			
