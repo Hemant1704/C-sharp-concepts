3a60ae41fd3480bd8009cc4a31ab0bf43f002da3
HEMANT CHAUHAN <18bit0032@gmail.com>
2021-10-17 23:22:46 +0530

    Adding some part of program7 about namespaces

---
+++ Program7.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lecture_1
+{
+	class Program7{
+		/*
+		In this we are going to learn about namespaces in this file.
+		Namespaces : They help us to organise our code. Example suppose i have 2 teams working on a project and
+		they are working on 2 different things - TeamA and TeamB. So i want to distinguish that which class is 
+		made by which team. With the help of namespaces i can do that easily.
+		 */
+		static void Main(string[] args) { 
+			
+		}
+	}
+}
